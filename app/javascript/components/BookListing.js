0f3ff84f770601329be19fbdaa6075b9454031ae
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 03:10:48 -0400

    [ah] added css for booklisting

--- app/javascript/components/BookListing.js
+++ app/javascript/components/BookListing.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import "../styles/BookListing.css"
+
 const BookListing = ({ book }) => {
   const { title, author, quantity } = book
 
@@ -14,11 +16,11 @@ const BookListing = ({ book }) => {
   }
 
   return (
-    <div>
-      <div>{title}</div>
-      <div>{author}</div>
-      <div>{quantityForDisplay(quantity)}</div>
-      <button>Reserve</button>
+    <div className="listing-container">
+      <div className="listing-title">{title}</div>
+      <div className="listing-author">{author}</div>
+      <div className="listing-quantity">{quantityForDisplay(quantity)}</div>
+      <button className="listing-reserve">Reserve</button>
     </div>
   )
 }
