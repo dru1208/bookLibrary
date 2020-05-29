1b02fb63f0730c71e5b536efd03db14274e7b43f
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 03:00:40 -0400

    [ah] book listing conditionally renders copies available text

--- app/javascript/components/BookListing.js
+++ app/javascript/components/BookListing.js
@@ -1,11 +1,23 @@
 import React from "react"
 
 const BookListing = ({ book }) => {
+  const { title, author, quantity } = book
+
+  const quantityForDisplay = (count) => {
+    if (count > 1) {
+      return `${count} copies available`
+    } else if (count === 1) {
+      return `1 copy available`
+    } else {
+      return "No copies available"
+    }
+  }
+
   return (
     <div>
-      <div>{book.title}</div>
-      <div>{book.author}</div>
-      <div>{book.quantity}</div>
+      <div>{title}</div>
+      <div>{author}</div>
+      <div>{quantityForDisplay(quantity)}</div>
       <button>Reserve</button>
     </div>
   )
