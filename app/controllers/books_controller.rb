46ff9c46a67f3e65ea0ae432942bdd220257afd9
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 06:28:13 -0400

    [ah] removed commented code

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,11 +1,5 @@
 class BooksController < ActionController::Base
   def index
-    # raw_page = params[:page].to_i
-    # page = raw_page > 0 ? raw_page : 1
-    # raw_query = params[:query]
-    # query = raw_query.present? ? raw_query : ''
-    # reserved = params[:reserved] == 'true'
-
     request = ::BookApiRequest.new(params[:page], params[:query], params[:reserved])
 
     book_library = ::BookLibrary.instance
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get '/books', to: 'books#index'
+  post '/books/reserve', to: 'books#reserve'
 
   get '*page', to: 'static#index', contraints: ->(req) do
     !req.xhr? && req.format.html?
