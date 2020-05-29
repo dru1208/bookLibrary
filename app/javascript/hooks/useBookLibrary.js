13131730d1f9cb277e36158f183e287fd4d92f4f
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 04:44:23 -0400

    [ah] extracted api call for updating library info to reusable function

--- app/javascript/hooks/useBookLibrary.js
+++ app/javascript/hooks/useBookLibrary.js
@@ -6,12 +6,16 @@ const useBookLibrary = () => {
   const [currentPage, setCurrentPage] = useState(1)
   const [totalPages, setTotalPages] = useState(1)
 
-  useEffect(() => {
-    axios.get(`/books?page=${currentPage}`).then((resp) => {
+  const bookApiCall = (page) => {
+    axios.get(`/books?page=${page}`).then((resp) => {
       const { data } = resp
       setBooks(data.books)
       setTotalPages(data.totalPages)
     })
+  }
+
+  useEffect(() => {
+    bookApiCall(currentPage)
   }, [])
 
   return {
