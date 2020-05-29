85780b07a6682c7e353e9b67d82cef1fb71a2ddc
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 05:00:36 -0400

    [ah] header css, render the inventory search bar

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,6 +2,9 @@ import React, { useState, useEffect } from "react"
 
 import useBookLibrary from "../hooks/useBookLibrary"
 
+import "../styles/App.css"
+
+import InventorySearch from "./InventorySearch.jsx"
 import BookListings from "./BookListings.jsx"
 import PageSelection from "./PageSelection.jsx"
 
@@ -15,7 +18,8 @@ const App = () => {
 
   return (
     <div>
-      <h1>Book Collection</h1>
+      <h1 className="header">Book Collection</h1>
+      <InventorySearch />
       <BookListings books={books} />
       <PageSelection
         totalPages={totalPages}
