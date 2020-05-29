52299de3c25c39ac14ef4cfe0b601b2b1c4f1825
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 02:23:13 -0400

    [ah] created initial api route that serves list of books

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/books', to: 'books#index'
 
   get '*page', to: 'static#index', contraints: ->(req) do
     !req.xhr? && req.format.html?
