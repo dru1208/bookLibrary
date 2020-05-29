453c8c5c60d1b8a7ce886f5df3f54d4b04429529
andrew hsieh <andrewhsieh1208@gmail.com>
2020-05-29 02:00:20 -0400

    [ah] set up first react component render

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get '*page', to: 'static#index', contraints: ->(req) do
+    !req.xhr? && req.format.html?
+  end
+
+  root 'static#index'
 end
